ec4131751042f013a25024339543b2f60d25e272
TylerJac <sans.gaming235@gmail.com>
2024-09-05 16:27:54 -0400

    added photo's and fixed the code

--- src/main/java/org/uma/uma/config/SecurityConfig.java
+++ src/main/java/org/uma/uma/config/SecurityConfig.java
@@ -1,10 +1,12 @@
 package org.uma.uma.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -14,7 +16,8 @@ import org.springframework.security.web.SecurityFilterChain;
 public class SecurityConfig {
 
     // Injecting custom UserDetailsService for authentication logic
-
+    @Autowired
+    private UserDetailsService userDetailsService;
     @Bean
     public PasswordEncoder passwordEncoder() {
         // Configures the password encoder to use BCrypt, a strong hashing algorithm
--- src/main/java/org/uma/uma/entity/User.java
+++ src/main/java/org/uma/uma/entity/User.java
@@ -13,7 +13,7 @@ import java.util.Set;
 @Getter
 @Setter
 @RequiredArgsConstructor
-@Table(name = "app_user")
+@Table(name = "users")
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
